30d865070fc2f4553cc3361a8cedda135becae5b
hy1203 <yaach123@naver.com>
2023-07-05 11:33:53 +0900

    travel mbti test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import TravleTest from "./pages/TravelTest";
+import TravelResult from "./pages/TravelResult";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<TravleTest />} />
+        <Route path="/Result" element={<TravelResult />} />
+      </Routes>
     </div>
   );
 }
