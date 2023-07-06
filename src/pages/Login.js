1b12f275940b519b7ef0e22e9b666c4b82059d70
dae <cys964@gmail.com>
2023-07-06 11:24:49 +0900

    로그인 폼에 헤더 추가

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,6 +4,7 @@ import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
 import axios from 'axios';
 import { Navigate, useNavigate } from 'react-router-dom';
+import MainHeader from './Header';
 
 const LoginHeader = styled.h1`
     font: bold 35px/1 'Noto Sans KR';
@@ -125,6 +126,7 @@ function Login() {
     return (
         <>
             <Wrapper>
+                <MainHeader />
                 <Inner>
                     <form onSubmit={handleSubmit(onSubmit)}>
                         <Filed>
@@ -133,11 +135,14 @@ function Login() {
 
                             <Label htmlFor="user_id">아이디:</Label>
                             <InputContainer>
-                                 <Input
-                  name="user_id"
-                  placeholder="아이디"
-                  {...register('user_id', { required: '아이디를 입력해주세요', maxLength: { value: 10, message: "아이디는 10자 이하로 만들어주세요," } })}
-                />
+                                <Input
+                                    name="user_id"
+                                    placeholder="아이디"
+                                    {...register('user_id', {
+                                        required: '아이디를 입력해주세요',
+                                        maxLength: { value: 10, message: '아이디는 10자 이하로 만들어주세요,' },
+                                    })}
+                                />
                             </InputContainer>
                             {errors.user_id && <Warn>{errors.user_id.message}</Warn>}
 
