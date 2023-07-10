719caeb846eb2f92ab9b59a0a4505593e9e02478
dae <cys964@gmail.com>
2023-07-10 15:16:54 +0900

    travel 오류 투성이 수정

--- src/pages/TravelTest.js
+++ src/pages/TravelTest.js
@@ -4,18 +4,20 @@ import { useNavigate } from 'react-router-dom';
 import Questions from '../common/api/questionApi.json';
 import Footer from './Footer';
 import MainHeader from './Header';
+
 function TravleTest() {
     const [currentPage, setCurrentPage] = useState(1);
     const [id, setId] = useState(1);
     const [num, setNum] = useState(0);
     const [mbtiList, setMbtiList] = useState([]);
     const navigate = useNavigate();
+
     if (sessionStorage.getItem('user_data') === null) {
         alert('로그인 후 이용해주세요');
         window.location.href = '/Login';
-        return;
+        return null;
     }
-    //배열 자르기 함수
+
     const chunk = (data = [], size = 1) => {
         const arr = [];
         for (let i = 0; i < data.length; i += size) {
@@ -23,27 +25,28 @@ function TravleTest() {
         }
         return arr;
     };
-    //위에 버튼 눌렀을 때
+
     const nextSlide1 = () => {
-        setMbtiList(mbtiList + Questions[num].a[0].type);
+        setMbtiList([...mbtiList, Questions[num].a[0].type]);
         setId(id + 1);
         setNum(num + 1);
         setCurrentPage(currentPage + 1);
     };
-    //아래 버튼 눌렀을 때
+
     const nextSlide2 = () => {
-        setMbtiList(mbtiList + Questions[num].a[1].type);
+        setMbtiList([...mbtiList, Questions[num].a[1].type]);
         setId(id + 1);
         setNum(num + 1);
         setCurrentPage(currentPage + 1);
     };
-    let result = [];
-    let i = 0;
-    let s = 0;
-    let t = 0;
-    let p = 0;
+
     if (id === 13) {
-        result = chunk(mbtiList, 1);
+        const result = chunk(mbtiList, 1);
+        let i = 0;
+        let s = 0;
+        let t = 0;
+        let p = 0;
+
         for (let j = 0; j < 12; j++) {
             if (result[j] === 'I') {
                 i += 1;
@@ -55,10 +58,13 @@ function TravleTest() {
                 p += 1;
             }
         }
-        let final_mbti = [i >= 2 ? 'I' : 'E', s >= 2 ? 'S' : 'N', t >= 2 ? 'T' : 'F', p >= 2 ? 'P' : 'J'];
+
+        const final_mbti = [i >= 2 ? 'I' : 'E', s >= 2 ? 'S' : 'N', t >= 2 ? 'T' : 'F', p >= 2 ? 'P' : 'J'];
+
         const mbti = final_mbti.join('');
         navigate('/Result', { state: { id: { mbti } } });
     }
+
     return (
         <div className="mainLayout">
             <MainHeader />
@@ -69,28 +75,27 @@ function TravleTest() {
                         <div>{`${currentPage} / ${Questions.length}`}</div>
                     </div>
                     {Questions.map((data) => {
-                        return (
-                            <>
-                                {data.id === id && (
-                                    <div className="questionBox" key={data.id}>
-                                        <div className="mbti_counter">
-                                            <h1 className="questionTitle">
-                                                Q{data.id}. {data.q}
-                                            </h1>
-                                        </div>
-                                        <div className="answerLayout">
-                                            <button type="button" className="answerButton" onClick={nextSlide1}>
-                                                {data.a[0].text}
-                                            </button>
-                                            <br />
-                                            <button type="button" className="answerButton" onClick={nextSlide2}>
-                                                {data.a[1].text}
-                                            </button>
-                                        </div>
+                        if (data.id === id) {
+                            return (
+                                <div className="questionBox" key={data.id}>
+                                    <div className="mbti_counter">
+                                        <h1 className="questionTitle">
+                                            Q{data.id}. {data.q}
+                                        </h1>
                                     </div>
-                                )}
-                            </>
-                        );
+                                    <div className="answerLayout">
+                                        <button type="button" className="answerButton" onClick={nextSlide1}>
+                                            {data.a[0].text}
+                                        </button>
+                                        <br />
+                                        <button type="button" className="answerButton" onClick={nextSlide2}>
+                                            {data.a[1].text}
+                                        </button>
+                                    </div>
+                                </div>
+                            );
+                        }
+                        return null;
                     })}
                 </div>
             </div>
