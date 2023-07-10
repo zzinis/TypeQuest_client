94640f2b21f48eb18ca649941adf1bd70ddfaf4f
dae <cys964@gmail.com>
2023-07-10 23:55:02 +0900

    useHistory 제거

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import LogoSrc from '../assets/TQ.png';
 //navigate
