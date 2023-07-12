0a8b120898df8c10362a62e2442e7cb41a2db0a7
ox01220122 <ox0122@gmail.com>
2023-07-12 15:20:49 +0900

    문의 관리 css, 가져오기

--- src/pages/Adminpage.js
+++ src/pages/Adminpage.js
@@ -11,26 +11,113 @@ const MainTitle = styled.div`
     display: flex;
     justify-content: center;
     padding-top: 2rem;
+    padding-bottom: 6rem;
 `;
 
-const commentWraper = styled.div`
-    background-color: rgba(42, 48, 96, 1);
-    color: white;
+const CommentWraper = styled.div`
+    width: 100%;
+    height: 100%;
     display: flex;
     justify-content: center;
-    width: 2rem;
-    height: 1rem;
+    align-items: center;
+    box-sizing: border-box;
 `;
-const commentBox = styled.input`
-    background: none;
-    background-color: rgba(42, 48, 96, 1);
+
+const CommentContainer = styled.div`
+    box-shadow: 0 2px 4px rgba(2, 2, 7, 0.8);
     color: white;
+    width: 70%;
+    height: 50%;
     display: flex;
     justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-bottom: 15rem; /* 각 박스 간격 조정 */
+    align-self: stretch;
+`;
+const CommentInput = styled.textarea`
+    background: none;
+    background-color: white;
+    border: none;
+    color: black;
+    display: flex;
+    justify-content: center;
+    width: 89.5%;
+    height: 9.8rem;
+    resize: none;
+    font-size: 1rem;
+`;
+
+const CommentBox = styled.div`
+    background: none;
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    padding: 0.05rem;
+`;
+
+const CommentExplain = styled.div`
+    border-top: 1px solid black;
+    background: none;
+    background-color: rgb(200, 200, 200, 0.5);
+    color: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    width: 10%;
+    height: auto;
+    padding: 0.1rem;
+`;
+
+const CommentContent = styled.div`
+    border-top: 1px solid black;
+    background: none;
+    background-color: rgb(48, 48, 48, 0.5);
+    color: white;
+    padding: 1.3rem;
+    width: 80%;
+    height: auto;
+    white-space: pre-wrap;
+    flex: 1;
+`;
+
+const CommentInputContent = styled.div`
+    border-top: 1px solid black;
+    background: none;
+    background-color: rgb(200, 200, 200, 0.5);
+    color: black;
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    width: 10%;
+    height: 10rem;
+`;
+
+const ButtonContainer = styled.div`
+    display: flex;
+`;
+
+const CommentSaveButton = styled.button`
+    width: auto;
+    height: 2.5rem;
+    color: white;
+    background: none;
+    background-color: gray;
+    margin: 0.5rem;
+    border-radius: 0.5rem;
+
+    &:hover {
+        background-color: rgb(50, 50, 50, 1); /* Change background color on hover */
+        cursor: pointer;
+    }
 `;
 
 const AdminPage = () => {
-    const [posts, setPosts] = useState([]);
+    const [posts, setPosts] = useState('');
     const [inquiries, setInquiries] = useState([]);
 
     useEffect(() => {
@@ -42,7 +129,30 @@ const AdminPage = () => {
             .catch((error) => {
                 console.error('문의 가져오기에 실패했습니다:', error);
             });
-    }, []);
+    }, [inquiries]);
+
+    const handleInputChange = (event) => {
+        setPosts(event.target.value);
+    };
+
+    const saveComment = (getAskId) => {
+        const ask_id = getAskId;
+        console.log(ask_id, '아아');
+        const data = {
+            ask_id,
+            comment: posts,
+        };
+
+        // axios
+        //     .patch(`${SERVER}/admincomment`)
+        //     .then((result) => {
+        //         setInquiries(result.data);
+        //     })
+        //     .catch((error) => {
+        //         console.error('문의 가져오기에 실패했습니다:', error);
+        //     });
+    };
+    const delComment = () => {};
 
     return (
         <div>
@@ -50,16 +160,44 @@ const AdminPage = () => {
             <MainTitle>문의 관리</MainTitle>
             {inquiries.map((inquirie) => (
                 <div key={inquirie.ask_id}>
-                    <div>{inquirie.title}</div>
-                    <div>{inquirie.content}</div>
-                    <commentWraper>
-                        <comment></comment>
-                    </commentWraper>
+                    <CommentWraper>
+                        <CommentContainer>
+                            <CommentBox>
+                                <CommentExplain>사용자ID</CommentExplain>
+                                <CommentContent>{inquirie.user_id}</CommentContent>
+                            </CommentBox>
+
+                            <CommentBox>
+                                <CommentExplain>제목</CommentExplain>
+                                <CommentContent>{inquirie.title}</CommentContent>
+                            </CommentBox>
+
+                            <CommentBox>
+                                <CommentExplain>문의글</CommentExplain>
+                                <CommentContent>{inquirie.content}</CommentContent>
+                            </CommentBox>
+
+                            <CommentBox>
+                                <CommentExplain>답변</CommentExplain>
+                                {inquirie.manager_msg ? (
+                                    <CommentContent>{inquirie.manager_msg}</CommentContent>
+                                ) : (
+                                    <CommentContent>none</CommentContent>
+                                )}
+                            </CommentBox>
+                            <CommentBox>
+                                <CommentInputContent>답변작성</CommentInputContent>
+                                <CommentInput onChange={handleInputChange}></CommentInput>
+                            </CommentBox>
+
+                            <ButtonContainer>
+                                <CommentSaveButton onClick={() => saveComment(inquirie.ask_id)}>저장</CommentSaveButton>
+                                <CommentSaveButton onClick={delComment}>문의글 삭제</CommentSaveButton>
+                            </ButtonContainer>
+                        </CommentContainer>
+                    </CommentWraper>
                 </div>
             ))}
-            <commentWraper>
-                <comment></comment>
-            </commentWraper>
             <Footer></Footer>
         </div>
     );
